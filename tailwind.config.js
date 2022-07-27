268ec35837fc4057cd16aaca39bde99d5ac25f3a
Djanilson Martins <djanlm@hotmail.com>
2022-07-27 13:23:44 -0400

    Add to cart implemented

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{html,js,ts,jsx,tsx}",
-    "./components/**/*.{html,js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
   },
