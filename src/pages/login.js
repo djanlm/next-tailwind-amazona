2b6cca4d4165dd9a929403f36d94ef0b3d63bafa
Djanilson Martins <djanlm@hotmail.com>
2022-07-28 05:56:35 -0400

    login page created

---
+++ src/pages/login.js
@@ -0,0 +1,68 @@
+import React from "react";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import Layout from "../components/Layout";
+
+export default function LoginScreen() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
+  const submitHandler = ({ email, password }) => {};
+  return (
+    <Layout title="Login">
+      <form
+        className="mx-auto max-w-screen-md"
+        onSubmit={handleSubmit(submitHandler)}
+      >
+        <h1 className="mb-4 text-xl">Login</h1>
+        <div className="mb-4">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            {...register("email", {
+              required: "Please enter e-mail",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "invalid email address",
+              },
+            })}
+            id="email"
+            className="w-full"
+            autoFocus
+          />
+          {errors.email && (
+            <div className="text-red-500">{errors.email.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            {...register("password", {
+              required: "Please enter password",
+              minLength: {
+                value: 6,
+                message: "password needs more than 5 chars",
+              },
+            })}
+            id="password"
+            className="w-full"
+            autoFocus
+          />
+          {errors.password && (
+            <div className="text-red-500">{errors.password.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <button className="primary-button">Login</button>
+        </div>
+        <div className="mb-4">
+          Don&apos;t have an account? <Link href="register"> Register</Link>
+        </div>
+      </form>
+    </Layout>
+  );
+}
