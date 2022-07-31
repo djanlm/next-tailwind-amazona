95ee9fd11299ee87813d94196d6ba984fb80dfe4
Djanilson Martins <djanlm@hotmail.com>
2022-07-31 12:27:38 -0400

    pay order by paypal

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,22 @@
-import "../styles/globals.css";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 import { SessionProvider, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import "../styles/globals.css";
 import { StoreProvider } from "../utils/Store";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <StoreProvider>
-        {Component.auth ? (
-          <Auth>
+        <PayPalScriptProvider deferLoading={true}>
+          {Component.auth ? (
+            <Auth>
+              <Component {...pageProps} />
+            </Auth>
+          ) : (
             <Component {...pageProps} />
-          </Auth>
-        ) : (
-          <Component {...pageProps} />
-        )}
+          )}
+        </PayPalScriptProvider>
       </StoreProvider>
     </SessionProvider>
   );
